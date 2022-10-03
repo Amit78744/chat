099d888cbbfa063a5b3206379453f8d85b26292f
ankitpansuria <ankit.pansuria@anviya.in>
2022-10-03 11:18:44 +0530

    add last message time in payload of chat message push notification.

---
+++ scripts/application_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source /home/ec2-user/.bash_profile
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/chat
+
+#navigate into our working directory where we have all our github files
+cd /home/ec2-user/socket
+
+#install node modules
+npm install
+npm start
+
+#start our node app in the background 
+node app.js > app.out.log 2> app.err.log < /dev/null & 
